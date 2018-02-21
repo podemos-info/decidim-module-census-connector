79feadb763946508b3e12e325bebe18459b4f912
David Rodríguez <deivid.rodriguez@riseup.net>
2018-02-21 08:30:17 -0300

    Only users with dni or nie over 18 can collaborate

--- app/models/decidim/census_connector/person.rb
+++ app/models/decidim/census_connector/person.rb
@@ -31,7 +31,28 @@ module Decidim
         @born_at ||= Date.parse(person_data.born_at)
       end
 
+      def age
+        @age ||= calculate_age
+      end
+
       private
+
+      def calculate_age
+        now = Time.zone.now.to_date
+
+        this_year = now.year
+        this_month = now.month
+        today = now.day
+
+        birth_year = born_at.year
+        birth_month = born_at.month
+        birth_day = born_at.day
+
+        month_of_birth_passed = this_month > birth_month
+        month_of_birth_current_but_day_passed = this_month == birth_month && today >= birth_day
+
+        this_year - birth_year - (month_of_birth_passed || month_of_birth_current_but_day_passed ? 0 : 1)
+      end
 
       attr_reader :person_data
     end
--- lib/decidim/census_connector/verifications/census/action_authorizer.rb
+++ lib/decidim/census_connector/verifications/census/action_authorizer.rb
@@ -5,20 +5,76 @@ module Decidim
     module Verifications
       module Census
         class ActionAuthorizer < Decidim::Verifications::DefaultActionAuthorizer
-          attr_reader :allowed_postal_codes
-
           def authorize
-            # Remove the additional setting from the options hash to avoid to be considered missing.
-            @membership_level ||= options.delete("membership_level")
-            @scope ||= options.delete("scope")
+            @allowed_document_types = options.delete("allowed_document_types")
+            @minimum_age = options.delete("minimum_age")
 
             status_code, data = *super
+
+            return [status_code, data] if status_code == :missing
+
+            if minimum_age.present? && age < minimum_age
+              status_code = :unauthorized
+              add_unmatched_field(data, "age" => age)
+            end
+
+            if allowed_document_types.present? && document_type == "passport"
+              status_code = :unauthorized
+              add_unmatched_field(data, "document_type" => document_type_label)
+            end
+
+            if minimum_age.present? || allowed_document_types.present?
+              data[:extra_explanation] = {
+                key: "extra_explanation",
+                params: {
+                  scope: "decidim.census_connector.verifications.census",
+                  minimum_age: minimum_age,
+                  allowed_documents: allowed_document_types.to_sentence(words_connector: " #{I18n.t("or")} ")
+                }
+              }
+            end
+
             [status_code, data]
           end
 
           # Adds the list of allowed postal codes to the redirect URL, to allow forms to inform about it
           def redirect_params
-            { "postal_codes" => allowed_postal_codes&.join("-") }
+            {
+              "document_types" => allowed_document_types&.join("-"),
+              "mimimum_age" => minimum_age
+            }
+          end
+
+          private
+
+          def add_unmatched_field(data, field)
+            data[:fields] ||= {}
+
+            data[:fields].merge!(field)
+          end
+
+          def age
+            person.age
+          end
+
+          def document_type
+            person.document_type
+          end
+
+          def document_type_label
+            I18n.t(document_type, scope: "census.api.person.document_type")
+          end
+
+          def allowed_document_types
+            @allowed_document_types.split(",").map(&:chomp)
+          end
+
+          def minimum_age
+            @minimum_age&.to_i
+          end
+
+          def person
+            PersonProxy.new(authorization.metadata["person_id"]).person
           end
         end
       end
