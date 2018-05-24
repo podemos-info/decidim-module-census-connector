ef9a29b42e8f3a0f6174248adb23c3618a4c8361
David Rodríguez <deivid.rodriguez@riseup.net>
2018-05-23 22:38:30 -0300

    Catch up scope specs with the recent refactorings

--- spec/lib/decidim/census_connector/seeds/scopes_spec.rb
+++ spec/lib/decidim/census_connector/seeds/scopes_spec.rb
@@ -6,41 +6,48 @@ require "decidim/census_connector/seeds/scopes"
 
 describe Decidim::CensusConnector::Seeds::Scopes do
   describe "#seed" do
-    subject(:method) { described_class.seed organization, base_path: base_path }
-
-    before { FileUtils.rm_rf(described_class::CACHE_PATH) }
+    before do
+      FileUtils.rm_rf(described_class::CACHE_PATH)
+      ENV["SCOPES_CACHE_PATH"] = nil
+    end
 
     let(:organization) { create(:organization) }
     let(:base_path) { File.expand_path("../../../../fixtures/seeds/scopes", __dir__) }
-    let(:instance) { described_class.instance }
+    let(:instance) { described_class.new(organization) }
 
     it "loads scopes data" do
-      expect { subject } .to change { Decidim::Scope.count } .from(0).to(20)
+      expect { instance.seed(base_path: base_path) } .to change { Decidim::Scope.count } .from(0).to(20)
     end
 
     it "loads scope types data" do
-      expect { subject } .to change { Decidim::ScopeType.count } .from(0).to(7)
+      expect { instance.seed(base_path: base_path) } .to change { Decidim::ScopeType.count } .from(0).to(7)
     end
 
     it "loads scope data from files" do
       expect(instance).to receive(:save_scope).at_least(1)
-      subject
+
+      instance.seed(base_path: base_path)
+    end
+
+    it "caches scopes after loading originals" do
+      expect { instance.seed(base_path: base_path) } .to change { File.exist?(described_class::CACHE_PATH) } .from(false).to(true)
     end
 
     context "when data is cached" do
       before do
-        described_class.seed organization, base_path: base_path
-        described_class.cache_scopes(CACHE_PATH)
+        instance.seed(base_path: base_path)
+
         Decidim::Scope.delete_all
       end
 
       it "load cached scopes data" do
-        expect { subject } .to change { Decidim::Scope.count } .from(0).to(20)
+        expect { instance.seed(base_path: base_path) } .to change { Decidim::Scope.count } .from(0).to(20)
       end
 
       it "doesn't load scope data from files" do
         expect(instance).not_to receive(:save_scope)
-        subject
+
+        instance.seed(base_path: base_path)
       end
     end
   end
