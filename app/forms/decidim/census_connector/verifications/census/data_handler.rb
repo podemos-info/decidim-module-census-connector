931530c3b1fa57b3f712e47c80f67709cbff5b55
David Rodríguez <deivid.rodriguez@riseup.net>
2018-05-24 19:07:17 -0300

    A bit less method chaining

--- app/forms/decidim/census_connector/verifications/census/data_handler.rb
+++ app/forms/decidim/census_connector/verifications/census/data_handler.rb
@@ -7,6 +7,8 @@ module Decidim
         class DataHandler < CensusHandler
           mimic :data_handler
 
+          delegate :email, to: :user
+
           def self.document_scopes
             @document_scopes ||= Decidim::Scope.top_level.order(name: :asc)
           end
