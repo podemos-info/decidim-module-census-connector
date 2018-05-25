458039f498d9314ecb62ec99d8c53f3188b538c1
David Rodríguez <deivid.rodriguez@riseup.net>
2018-05-25 11:51:41 -0300

    Minor style tweak
    This is the most standard style even though rubocop is not able to
    enforce it.

--- app/forms/decidim/census_connector/verifications/census/data_form.rb
+++ app/forms/decidim/census_connector/verifications/census/data_form.rb
@@ -78,7 +78,7 @@ module Decidim
           end
 
           def local_document?
-            Person.local_document? document_type
+            Person.local_document?(document_type)
           end
 
           def verified?
